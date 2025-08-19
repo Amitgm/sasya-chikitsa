765c9a9d803ee6173dfe6b7fca6795385eb8231d
Rajiv Ranjan <rajranja@redhat.com>
2025-08-19 23:56:39 +0530

    added api to container and deployed on openshift

--- server/api/server.py
+++ server/api/server.py
@@ -40,4 +40,4 @@ async def hello():
 
 if __name__ == "__main__":
     # Start the FastAPI server at localhost:8080
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
