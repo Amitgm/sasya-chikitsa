75a832a713f32d9c2e903898a31d8210f890fc34
techmoksha <adbrihadaranyaka@gmail.com>
2025-08-19 08:57:29 +0530

    First cut basic model integration with API.

---
+++ ml/cnn_with_attention_classifier.py
@@ -0,0 +1,98 @@
+import base64
+import numpy as np
+import cv2
+import os
+from tensorflow.keras.models import load_model
+from keras.layers import Layer
+import tensorflow as tf
+
+LABEL_CLASSES = [
+    'Apple_scab', 'Bacterial_spot', 'Black_rot', 'Cedar_apple_rust',
+    'Cercospora_leaf_spot Gray_leaf_spot', 'Common_rust', 'Early_blight',
+    'Esca_(Black_Measles)', 'Haunglongbing_(Citrus_greening)', 'Late_blight',
+    'Leaf_Mold', 'Leaf_blight_(Isariopsis_Leaf_Spot)', 'Leaf_scorch',
+    'Northern_Leaf_Blight', 'Powdery_mildew', 'Septoria_leaf_spot',
+    'Spider_mites Two-spotted_spider_mite', 'Target_Spot',
+    'Tomato_Yellow_Leaf_Curl_Virus', 'Tomato_mosaic_virus', 'Unknown', 'healthy'
+]
+
+TARGET_SIZE = (64, 64)
+
+# Define the custom layer needed for loading the model
+class ReshapeLayer(Layer):
+    def __init__(self, target_shape, **kwargs):
+        super(ReshapeLayer, self).__init__(**kwargs)
+        self.target_shape = target_shape
+
+    def call(self, inputs):
+        return tf.reshape(inputs, self.target_shape)
+
+    def get_config(self):
+        config = super(ReshapeLayer, self).get_config()
+        config.update({
+            'target_shape': self.target_shape,
+        })
+        return config
+
+class CNNWithAttentionClassifier(Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.loaded_model = CNNWithAttentionClassifier.load_model()
+        print("[INFO] Model loaded successfully.")
+
+    @staticmethod
+    def load_model():
+        ml_dir = os.path.dirname(__file__)
+        parent_dir = os.path.dirname(ml_dir)
+        resources_dir = os.path.join(parent_dir, "resources")
+        binary_model_path = os.path.join(resources_dir, "leaf_classification_attention_cnn_model.h5")
+        print(f"[INFO] Loading binary leaf presence model from {binary_model_path}...")
+        return load_model(binary_model_path, custom_objects={'ReshapeLayer': ReshapeLayer})
+
+
+    def predict_leaf_classification(self, image_bytes, input_text=""):
+        """
+        Predicts whether an image contains a leaf using the trained classification model.
+
+        Args:
+            image_bytes (str): Base64-encoded image bytes.
+            input_text (str): Optional additional text.
+        Yields:
+            str: Intermediate output strings.
+        """
+        if self.loaded_model is None:
+            yield "Error: Model is not loaded.\n"
+            return
+
+        if image_bytes is None:
+            yield "Error: Mandatory argument 'image_bytes' is missing.\n"
+            return
+
+        try:
+            image_decoded = base64.b64decode(image_bytes)
+            nparr = np.frombuffer(image_decoded, np.uint8)
+            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        except Exception as e:
+            yield f"Error: Could not decode base64 image ({str(e)})\n"
+            return
+        if image is None:
+            yield "Error: Could not load image from bytes.\n"
+            return
+
+        image_resized = cv2.resize(image, TARGET_SIZE)
+        yield "Resized image, normalizing and preprocessing...\n"
+
+        image_preprocessed = image_resized.astype(np.float32) / 255.0
+        image_for_prediction = np.expand_dims(image_preprocessed, axis=0)
+
+        prediction = self.loaded_model.predict(image_for_prediction)
+        yield f"Making prediction...{prediction}\n"
+
+        predicted_class_index = np.argmax(prediction)
+        predicted_class_label = LABEL_CLASSES[predicted_class_index]
+        yield f"Predicted class label...{predicted_class_label}\n"
+
+        prediction_probability = prediction[0][predicted_class_index]
+        yield f"Prediction made. Class: {predicted_class_label}, Probability: {prediction_probability}\n"
+        return
